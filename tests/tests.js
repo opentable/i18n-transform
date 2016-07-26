29ab37c31f53abe63be00658d18ede420dec3cfd
Christopher Cartlidge <ccartlidge@opentable.com>
2016-07-26 10:47:35 +0100

    Selects primary langauge if match between iso 639-1

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,8 @@
 var i18n = require('../lib/i18n-transform');
-    should = require('should');
+should = require('should');
 
-describe('i18n tests', function(){
-    it('should merge the fields for the specified language', function(){
+describe('i18n tests', function () {
+    it('should merge the fields for the specified language', function () {
         var result = i18n.transform({
             i18n: [
                 {
@@ -24,14 +24,14 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "en", region: "US", quality: 1.0 }
-        ]);
+                { code: "en", region: "US", quality: 1.0 }
+            ]);
 
         result.Name.should.eql("gonna drink some beer and shoot some stuff y'all");
         result.Language.IETF.should.eql("en-US");
     });
 
-    it('should allow us to use I18n instead of i18n for the international array', function(){
+    it('should allow us to use I18n instead of i18n for the international array', function () {
         var result = i18n.transform({
             I18n: [
                 {
@@ -53,46 +53,46 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "en", region: "US", quality: 1.0 }
-        ]);
+                { code: "en", region: "US", quality: 1.0 }
+            ]);
 
         result.Name.should.eql("gonna drink some beer and shoot some stuff y'all");
         result.Language.IETF.should.eql("en-US");
     });
 
-    it('should add the language fields to the destination when using transformDestination', function(){
-      var destinationObject = {};
-      i18n.transformDestination({
-        i18n: [
-          {
-            Name: "gonna drink some beer and shoot some stuff y'all",
-            Language: {
-              IETF: "en-US",
-              Code: "en",
-              Region: "US"
-            }
-          },
-          {
-            Name: "pip pip tally ho crumpets and tea",
-            Language: {
-              IETF: "en-GB",
-              Code: "en",
-              Region: "GB"
-            }
-          }
-        ],
-        PrimaryLanguage: "en-US"
-      },
-      destinationObject,
-      [
-        { code: "en", region: "US", quality: 1.0 }
-      ]);
+    it('should add the language fields to the destination when using transformDestination', function () {
+        var destinationObject = {};
+        i18n.transformDestination({
+            i18n: [
+                {
+                    Name: "gonna drink some beer and shoot some stuff y'all",
+                    Language: {
+                        IETF: "en-US",
+                        Code: "en",
+                        Region: "US"
+                    }
+                },
+                {
+                    Name: "pip pip tally ho crumpets and tea",
+                    Language: {
+                        IETF: "en-GB",
+                        Code: "en",
+                        Region: "GB"
+                    }
+                }
+            ],
+            PrimaryLanguage: "en-US"
+        },
+            destinationObject,
+            [
+                { code: "en", region: "US", quality: 1.0 }
+            ]);
 
-      destinationObject.Name.should.eql("gonna drink some beer and shoot some stuff y'all");
-      destinationObject.Language.IETF.should.eql("en-US");
+        destinationObject.Name.should.eql("gonna drink some beer and shoot some stuff y'all");
+        destinationObject.Language.IETF.should.eql("en-US");
     });
 
-    it('should preserve fields in the root of the document', function(){
+    it('should preserve fields in the root of the document', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -115,15 +115,15 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "en", region: "US", quality: 1.0 }
-        ]);
+                { code: "en", region: "US", quality: 1.0 }
+            ]);
 
         result.DomainId.should.eql(123);
     });
 
-    it('should select the primary language when no language is specified', function(){
+    it('should select the primary language when no language is specified', function () {
         var result = i18n.transform({
-            i18n:[
+            i18n: [
                 {
                     Name: "gonna drink some beer and shoot some stuff y'all",
                     Language: {
@@ -148,7 +148,7 @@ describe('i18n tests', function(){
         result.Language.IETF.should.eql("en-US");
     });
 
-    it('should select the first available language based on the given set', function(){
+    it('should select the first available language based on the given set', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -171,14 +171,14 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "es", region: "MX", quality: 1.0 },
-            { code: "en", region: "US", quality: 0.8 }
-        ]);
+                { code: "es", region: "MX", quality: 1.0 },
+                { code: "en", region: "US", quality: 0.8 }
+            ]);
 
         result.Language.IETF.should.eql("en-US");
     });
 
-    it('should select the primary language when a wildcard is specified', function(){
+    it('should select the primary language when a wildcard is specified', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -201,15 +201,15 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "es", region: "MX", quality: 1.0 },
-            { code: "fr", region: "CA", quality: 0.8 },
-            { code: "*", quality: 0.4 }
-        ]);
+                { code: "es", region: "MX", quality: 1.0 },
+                { code: "fr", region: "CA", quality: 0.8 },
+                { code: "*", quality: 0.4 }
+            ]);
 
         result.Language.IETF.should.eql("en-US");
     });
 
-    it('should select the primary language when a wildcard is specified anywhere in the set', function(){
+    it('should select the primary language when a wildcard is specified anywhere in the set', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -232,46 +232,16 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "es", region: "MX", quality: 1.0 },
-            { code: "*", quality: 0.8 },
-            { code: "fr", region: "CA", quality: 0.6 },
-            { code: "en", region: "GB", quality: 0.4 }
-        ]);
+                { code: "es", region: "MX", quality: 1.0 },
+                { code: "*", quality: 0.8 },
+                { code: "fr", region: "CA", quality: 0.6 },
+                { code: "en", region: "GB", quality: 0.4 }
+            ]);
 
         result.Language.IETF.should.eql("en-US");
     });
 
-    it('should select the first match when there are multiple matches', function(){
-        var result = i18n.transform({
-            DomainId: 123,
-            i18n: [
-                {
-                    Name: "pip pip tally ho crumpets and tea",
-                    Language: {
-                        IETF: "en-GB",
-                        Code: "en",
-                        Region: "GB"
-                    }
-                },
-                {
-                    Name: "gonna drink some beer and shoot some stuff y'all",
-                    Language: {
-                        IETF: "en-US",
-                        Code: "en",
-                        Region: "US"
-                    }
-                }
-            ],
-            PrimaryLanguage: "en-US"
-        }, [
-            { code: "en", region: "AU", quality: 1.0 },
-            { code: "en", quality: 0.8 }
-        ]);
-
-        result.Language.IETF.should.eql("en-GB");
-    });
-
-    it('should select the first match from a complex set', function(){
+    it('should select the first match from a complex set', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -302,17 +272,17 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "de", region: "DE", quality: 1.0 },
-            { code: "fr",               quality: 0.8 },
-            { code: "en", region: "US", quality: 0.6 },
-            { code: "en",               quality: 0.4 },
-            { code: "*",                quality: 0.1 }
-        ]);
+                { code: "de", region: "DE", quality: 1.0 },
+                { code: "fr", quality: 0.8 },
+                { code: "en", region: "US", quality: 0.6 },
+                { code: "en", quality: 0.4 },
+                { code: "*", quality: 0.1 }
+            ]);
 
         result.Language.IETF.should.eql("fr-CA");
     });
 
-    it('should sort the language set based on quality', function(){
+    it('should sort the language set based on quality', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -343,17 +313,17 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "en", region: "US", quality: 0.6 },
-            { code: "de", region: "DE", quality: 1.0 },
-            { code: "en",               quality: 0.4 },
-            { code: "*",                quality: 0.1 },
-            { code: "fr",               quality: 0.8 },
-        ]);
+                { code: "en", region: "US", quality: 0.6 },
+                { code: "de", region: "DE", quality: 1.0 },
+                { code: "en", quality: 0.4 },
+                { code: "*", quality: 0.1 },
+                { code: "fr", quality: 0.8 },
+            ]);
 
         result.Language.IETF.should.eql("fr-CA");
     });
 
-    it('should compare language codes in a case-insensitive manner', function(){
+    it('should compare language codes in a case-insensitive manner', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -376,13 +346,13 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-GB"
         }, [
-            { code: "En", region: "us", quality: 1 }
-        ]);
+                { code: "En", region: "us", quality: 1 }
+            ]);
 
         result.Language.IETF.should.eql("en-US");
     });
 
-    it('should not break when region is undefined', function(){
+    it('should not break when region is undefined', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -403,13 +373,13 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en"
         }, [
-            { code: "En", region: "US", quality: 1 }
-        ]);
+                { code: "En", region: "US", quality: 1 }
+            ]);
 
         result.Language.IETF.should.eql("en");
     });
 
-    it('should delete the i18n section', function(){
+    it('should delete the i18n section', function () {
         var result = i18n.transform({
             i18n: [
                 {
@@ -423,13 +393,13 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "en", region: "US", quality: 1.0 }
-        ]);
+                { code: "en", region: "US", quality: 1.0 }
+            ]);
 
         (result.i18n === undefined).should.eql(true);
     });
 
-    it('should delete the I18n section if thats what we found', function(){
+    it('should delete the I18n section if thats what we found', function () {
         var result = i18n.transform({
             I18n: [
                 {
@@ -443,24 +413,24 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-US"
         }, [
-            { code: "en", region: "US", quality: 1.0 }
-        ]);
+                { code: "en", region: "US", quality: 1.0 }
+            ]);
 
         (result.I18n === undefined).should.eql(true);
     });
 
-    it('should not break when source.i18n is undefined', function(){
+    it('should not break when source.i18n is undefined', function () {
         var result = i18n.transform({
             AField: 123,
             PrimaryLanguage: "en"
         }, [
-            { code: "En", region: "US", quality: 1 }
-        ]);
+                { code: "En", region: "US", quality: 1 }
+            ]);
 
         result.AField.should.eql(123);
     });
 
-    it('should return null if preferred language does not exist', function(){
+    it('should return null if preferred language does not exist', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -475,37 +445,37 @@ describe('i18n tests', function(){
             ],
             PrimaryLanguage: "en-GB"
         }, [
-            { code: "de", region: "DE", quality: 1.0 }
-        ]);
+                { code: "de", region: "DE", quality: 1.0 }
+            ]);
 
         (result == null).should.be.true;
     });
 
 
-    it('transformDestination should give an error if language does not exist', function(){
+    it('transformDestination should give an error if language does not exist', function () {
         var error = null;
-            var result = i18n.transformDestination({
-                DomainId: 123,
-                i18n: [
-                    {
-                        Name: "pip pip tally ho crumpets and tea",
-                        Language: {
-                            IETF: "en-GB",
-                            Code: "en",
-                            Region: "GB"
-                        }
-                    },
-                ],
-                PrimaryLanguage: "en-GB"
-            }, {}, [
+        var result = i18n.transformDestination({
+            DomainId: 123,
+            i18n: [
+                {
+                    Name: "pip pip tally ho crumpets and tea",
+                    Language: {
+                        IETF: "en-GB",
+                        Code: "en",
+                        Region: "GB"
+                    }
+                },
+            ],
+            PrimaryLanguage: "en-GB"
+        }, {}, [
                 { code: "de", region: "DE", quality: 1.0 }
-            ], function(err){
+            ], function (err) {
                 error = err;
             });
         (error == null).should.be.false;
     });
 
-    it('if we ask for * without a primary language then just use whatever comes first', function(){
+    it('if we ask for * without a primary language then just use whatever comes first', function () {
         var result = i18n.transform({
             DomainId: 123,
             i18n: [
@@ -519,9 +489,71 @@ describe('i18n tests', function(){
                 },
             ]
         }, [
-            { code: "*", quality: 1.0 }
-        ]);
+                { code: "*", quality: 1.0 }
+            ]);
 
         result.Name.should.eql('pip pip tally ho crumpets and tea');
+    });
+
+    it('should select the primary language', function () {
+        var result = i18n.transform({
+            i18n: [
+                {
+                    Name: "[en-US] Name",
+                    Language: {
+                        IETF: "en-US",
+                        Code: "en",
+                        Region: "US"
+                    }
+                },
+                {
+                    Name: "[en-GB] Name",
+                    Language: {
+                        IETF: "en-GB",
+                        Code: "en",
+                        Region: "GB"
+                    }
+                }
+            ],
+            PrimaryLanguage: "en-GB"
+        }, [
+                { code: "de", region: "DE", quality: 1.0 },
+                { code: "de", region: undefined, quality: 1.0 },
+                { code: "en", region: undefined, quality: 1.0 }
+            ]
+        );
+
+        result.Language.IETF.should.eql("en-GB");
+    });
+
+    it('shouldn\'t select the primary language', function () {
+        var result = i18n.transform({
+            i18n: [
+                {
+                    Name: "[en-US] Name",
+                    Language: {
+                        IETF: "en-US",
+                        Code: "en",
+                        Region: "US"
+                    }
+                },
+                {
+                    Name: "[en-GB] Name",
+                    Language: {
+                        IETF: "en-GB",
+                        Code: "en",
+                        Region: "GB"
+                    }
+                }
+            ],
+            PrimaryLanguage: "en-GB"
+        }, [
+                { code: "de", region: "DE", quality: 1.0 },
+                { code: "de", region: undefined, quality: 1.0 },
+                { code: "en", region: 'US', quality: 1.0 }
+            ]
+        );
+
+        result.Language.IETF.should.eql("en-US");
     });
 });
